c31f41d6753d4c8ced9f2e79b84967edbbe29f18
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2026-01-28 21:13:25 +0530

    Update thumbnails.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- BABYMUSIC/utils/thumbnails.py
+++ BABYMUSIC/utils/thumbnails.py
@@ -1,4 +1,2 @@
-THUMB_BASE = "https://i.ytimg.com/vi"
-
-def get_thumb(videoid: str) -> str:
-    return f"{THUMB_BASE}/{videoid}/maxresdefault.jpg"
+def get_thumb(videoid, chat_id=None):
+    return f"https://i.ytimg.com/vi/{videoid}/mqdefault.jpg"
